ab80efd9083c366f56e828bc2ca25f66a18e72d4
DesdenLK <acostaiglesias@gmail.com>
2024-05-04 23:06:48 +0200

    Not working UI

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from taipy import Gui
 from datetime import datetime
 import taipy.gui.builder as tgb
+from taipy.gui import State
 import taipy as tp
+from taipy.gui import navigate
+
+
+from controller import getEvents
 
 cities = ['Amsterdam', 'Barcelona', 'Berlin', 'Brussels', 'Budapest', 'Dublin', 'Florence', 'Lisbon', 'London', 'Madrid', 'Milan', 'Munich', 'Paris', 'Prague', 'Rome', 'Vienna', 'Zurich']
 date_init = datetime.now()
 date_end = datetime.now()
+
 
 departure = ""
 arrival = ""
@@ -21,21 +27,34 @@ categories = list(categoryToActivity.keys())
 activities = []
 selected_activities = []
 selected_categories = []
-
+events = []
 
 init = True
 start_again = False
+all_fields = False
+
 def change_init(state):
     state.init = False
     state.start_again = True
-
+    #state.events = getEvents(state.date_init, state.date_end, state.departure, state.selected_activities)
+    state.events = getEvents(datetime(2024,6,4), datetime(2024,7,5), "Barcelona", ["Concerts"])
+    while state.events == []:
+        True
+    print("Events: ", state.events)
+    navigate(state, "feed", force=state.start_again)
+    print("Events 3: ", state.events)
 def default_values(state):
     state.init = True
     state.start_again = False
     state.departure = ""
     state.arrival = ""
+    navigate(state, "/")
+
+def all_fields_filled(state):
+    return state.departure != "" and state.arrival != "" and state.selected_categories != [] and state.selected_activities != []
 
 def on_change(state, var_name: str, value: any):
+    state.all_fields = all_fields_filled(state)
     if var_name == "departure":
         state.departure = value
     elif var_name == "arrival":
@@ -51,37 +70,48 @@ def on_change(state, var_name: str, value: any):
 
 
 
-with tgb.Page(on_refresh=change_init) as root_page:
-    tgb.text(value="# Welcome Traveler", mode="md", color="primary", align="center")
-    with tgb.layout("4*1"):
-        with tgb.part(render = "{init}"):
-            tgb.date("{date_init}", label="Departure Date", with_time=False)
-        with tgb.part(render = "{init}"):
-            tgb.date("{date_end}", label="Return Date", with_time=False)
-        with tgb.part(render = "{init}"): 
-            tgb.selector(value="{departure}",label="Departure City", lov="{cities}", dropdown=True, on_change=on_change)
-        with tgb.part(render = "{init}"):
-            tgb.selector(value="{arrival}",label="Arrival City", lov="{cities}", default="{arrival}",dropdown=True, on_change=on_change)
+with tgb.Page(on_refresh=default_values) as root_page:
     with tgb.part(render = "{init}"):
+        tgb.text(value="# Welcome Traveler", mode="md", color="primary", align="center")
+        with tgb.layout("4*1"):
+            tgb.date("{date_init}", label="Departure Date", with_time=False)
+            tgb.date("{date_end}", label="Return Date", with_time=False)
+            tgb.selector(value="{departure}",label="Departure City", lov="{cities}", dropdown=True, on_change=on_change)
+            tgb.selector(value="{arrival}",label="Arrival City", lov="{cities}", default="{arrival}",dropdown=True, on_change=on_change)
+
         tgb.text(value="#### \nSelect your preferences", mode="md", color="primary", align="center")
 
-    with tgb.layout("4*1"):
-        with tgb.part(render = "{init}"):
+        with tgb.layout("4*1"):
             tgb.selector(value= "{selected_categories}", label = "Categories", lov = "{categories}", multiple = True, dropdown=True, on_change=on_change)
-        with tgb.part(render = "{init}"):
             tgb.selector(value= "{selected_activities}", label = "Activities", lov = "{activities}", multiple = True, dropdown=True, on_change=on_change)
-    with tgb.part(render = "{init}"):
-        tgb.text(value="####\n", mode="md")
+        with tgb.part(render = "{init}"):
+            tgb.text(value="####\n", mode="md")
     
-    with tgb.part(render = "{init}"):
-        tgb.button("Meet People", color="primary", size="lg", block=True, on_action=change_init)
+        tgb.button("Meet People", color="primary", size="lg", block=True, on_action=change_init, active="{all_fields}")
+
+
+with tgb.Page(on_refresh=default_values) as feed:
     with tgb.part(render = "{start_again}"):
+        tgb.text(value="# Feed", mode="md", color="primary", align="center")
+        print("Events2: ", events)
+        for e,p in events:
+            with tgb.expandable("Event: " + e.name):
+                tgb.input(value=e.classification, active=False, label="Classification")
+                tgb.text(value="Date: " + e.date)
+                tgb.text(value="Time: " + e.time)
+                tgb.text(value="Venue: " + e.venue)
+                
         tgb.button("Start Again", color="secondary", size="lg", block=True, on_action=default_values, on_change=on_change)
 
 
+
+    
+
+
 pages = {
-    "/" : root_page
+    "/" : root_page,
+    "feed" : feed
 }
 
 if __name__ == '__main__':
-    Gui(pages=pages).run(debug=True, use_reloader=True, )
+    Gui(pages=pages).run(debug=True, use_reloader=True)
