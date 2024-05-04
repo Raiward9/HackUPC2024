b19e9ec6fa961541e9f978c9b3b3330f915caf74
sergi.martinez.pamias <sergi.martinez.pamias@estudiantat.upc.edu>
2024-05-04 16:45:50 +0200

    Finished querying

--- testQuery.py
+++ testQuery.py
@@ -1,26 +1,42 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from pymongo.cursor import Cursor
 import datetime
 
 cities = ['Amsterdam', 'Barcelona', 'Berlin', 'Brussels', 'Budapest', 'Dublin', 'Florence', 'Lisbon', 'London', 'Madrid', 'Milan', 'Munich', 'Paris', 'Prague', 'Rome', 'Vienna', 'Zurich']
 interests = ['Art Exhibitions', 'Beach Volleyball', 'Concerts', 'Cooking Classes', 'Cultural Festivals', 'Dancing Classes', 'Escape Rooms', 'Food Tours', 'Football Matches', 'Group Cycling Tours', 'Hiking Excursions', 'Hot Air Balloon Rides', 'Karaoke Nights', 'Kayaking', 'Pub Crawls', 'River Cruises', 'Rock Climbing', 'Scavenger Hunts', 'Segway Tours', 'Sightseeing Tours', 'Snorkeling Tours', 'Theater Performances', 'Theme Parks', 'Wine Tasting', 'Zip Line Adventures']
 
+DEBUG = False
+dbClient = None
 
-def getDbClient() -> MongoClient:
+def initDbClient():
     uri = "mongodb+srv://sergimartinezpamias:FwIFc70b407VBCuw@smp.16uhelv.mongodb.net/?retryWrites=true&w=majority&appName=SMP"
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
     # Send a ping to confirm a successful connection
     try:
         client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-        return client
+        if DEBUG: print("Pinged your deployment. You successfully connected to MongoDB!")
+        global dbClient
+        dbClient = client
     except Exception as e:
         print(e)
         return None
 
 
-def doQuery(collection, depDate:datetime.datetime, retDate:datetime.datetime, city:str, preferences:list[str]):
+def doQuery(depDate:datetime.datetime, retDate:datetime.datetime, city:str, preferences:list[str]) -> Cursor:
+    global dbClient
+    ntries = 0
+    while (dbClient == None and ntries < 5):
+        initDbClient()
+        ntries += 1
+        
+    if ntries == 5:
+        raise Exception("Failed connection")
+    
+    db = dbClient['hackupc_travel']
+    collection = db['travel_info']
+    
     if not city in cities:
         print("Invalid city", city, ", not in", cities)
         return None
@@ -62,7 +78,7 @@ def askForCity(prompt:str) -> str:
 def askForInterests(prompt:str)->list[str]:
     retlist = []
     print(prompt)
-    while (input("Any more interests [y/n]") != "n"):
+    while (input("Current interests: " + str(retlist) + ";\nAny more interests [y/n]") != "n"):
         int_input = input(prompt + "Choose from: " + str(interests) + "\n")    
         if not int_input in interests:
             print("Interest not available")
@@ -73,20 +89,13 @@ def askForInterests(prompt:str)->list[str]:
             
 
 def main():
-    client = getDbClient()
-    if client == None:
-        print("Connection failed!")
-        return
-    
-    db = client['hackupc_travel']
-    collection = db['travel_info']
     
     depDate = askForDate("Please enter an arrival date (YYYY-MM-DD): ")
     retDate = askForDate("Please enter a return date (YYYY-MM-DD): ")
     city = askForCity("Please enter arrival city; ")
     profile = askForInterests("Please enter your interests; ")
         
-    results = doQuery(collection, depDate, retDate, city, profile)
+    results = doQuery(depDate, retDate, city, profile)
     
     for res in results:
         print(res)
