76092697bf04fe1ba157596d2bba1efdcc66486a
albertpnet <albertplbusiness@gmail.com>
2024-05-04 17:29:47 +0200

    eventsAPI updated to return the list of events for a given city and date

--- eventsAPI.py
+++ eventsAPI.py
@@ -1,9 +1,10 @@
 import requests
 from datetime import datetime
+from dataclasses import dataclass
 
 uri = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=yhLUu4K12SkgG9JHcbG3qvrsx4WehgdU"
 
-# Your Ticketmaster API key
+# Ticketmaster API key
 API_KEY = 'yhLUu4K12SkgG9JHcbG3qvrsx4WehgdU'
 
 # Base URL for Ticketmaster API
@@ -12,14 +13,25 @@ BASE_URL = 'https://app.ticketmaster.com/discovery/v2/'
 # Endpoint for searching events
 SEARCH_EVENTS_ENDPOINT = 'events.json'
 
-def showEventsInfo(events):
+@dataclass
+class Event:
+    name: str
+    type: str
+    date: str
+    time: str
+    venue: str
+
+def getEventsInfo(events):
+    eventsInfo = []
     for event in events:
-        print("Name:", event['name'])
-        print("Date:", event['dates']['start']['localDate'])
-        print("Time:", event['dates']['start']['localTime'])
-        print("Type:", event['classifications'][0]['segment']['name'])
-        print("Venue:", event['_embedded']['venues'][0]['name'])
-        print('----------------------------------------------')
+        name = event['name']
+        type = event['classifications'][0]['segment']['name']
+        date =  event['dates']['start']['localDate']
+        time = event['dates']['start']['localTime']
+        venue = event['_embedded']['venues'][0]['name']
+        eventsInfo.append(Event(name,type,date,time,venue))
+    
+    return eventsInfo
 
 
 def getActivitiesFromCityAndDate(cityName,date):
@@ -31,11 +43,11 @@ def getActivitiesFromCityAndDate(cityName,date):
     response = requests.get(BASE_URL + SEARCH_EVENTS_ENDPOINT, params=params)
     if response.status_code == 200:
         data = response.json()
-        showEventsInfo(data['_embedded']['events'])
+        return getEventsInfo(data['_embedded']['events'])
 
     else: print("Error while making a request to api")
 
-dateSt = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')  
 
-# testing
-getActivitiesFromCityAndDate('Syracuse',dateSt)
+# ---------------- TESTING ----------------
+#dateSt = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')  
+#print(getActivitiesFromCityAndDate('Syracuse',dateSt))
